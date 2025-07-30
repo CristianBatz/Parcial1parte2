2566065475a167a3e4c9b24153809ad26a7f4e1a
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-30 10:15:30 -0600

    se comenzo creando el menu con las 5 opciones ademas de comenzar con la funcion de cadena

---
+++ MenuMatematico.py
@@ -0,0 +1,17 @@
+def cadena_repetir(cantidad,i=0,palabra):
+    if i == cantidad:
+        return 0
+
+
+
+
+opcion = 0
+while opcion != 5:
+    print("=== Menu recursivo ===")
+    print("1. Calcular MCD ")
+    print("2. Crear cadena repetida")
+    print("3. Cantidad de una letra en una palabra")
+    print("4. Conversion decimal a binario")
+    print("5. Cantidad de digitos en un numero")
+    opcion = int(input("Seleccione una opcion: "))
+
