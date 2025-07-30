121ee91cb166eddb7bef639c7fc01829d782a433
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-30 11:27:08 -0600

    arreglando iteraciones

--- MenuMatematico.py
+++ MenuMatematico.py
@@ -1,34 +1,39 @@
-def calcular_mcd(numeroA,numeroB):
+def calcular_mcd(numeroA, numeroB):
     if numeroB == 0:
         return numeroA
     else:
         print(f"mcd:{numeroA},{numeroB}")
-        return calcular_mcd(numeroB, numeroA%numeroB)
+        return calcular_mcd(numeroB, numeroA % numeroB)
 
-def cadena_repetir(cantidad,palabra,i=0):
+
+def cadena_repetir(cantidad, palabra, i=0):
     if i == cantidad:
         return 0
     else:
         print(f"{palabra}")
-        return cadena_repetir(cantidad,palabra,i+1)
+        return cadena_repetir(cantidad, palabra, i + 1)
 
-def cantidad_letra(cadena,letra,i=0):
+
+def cantidad_letra(cadena, letra, i=0):
     if i == len(cadena):
         return 0
     else:
         return cadena.count(letra)
 
+
 def convertir_binario(numeroC):
     if numeroC == 0:
         return ""
     else:
-        return convertir_binario(numeroC//2) + str(numeroC%2)
+        return convertir_binario(numeroC // 2) + str(numeroC % 2)
 
-def calcular_digitos(numeroD,i=0):
+
+def calcular_digitos(numeroD, i=0):
     if i == len(numeroD):
         return i
     else:
-        return calcular_digitos(numeroD,i+1)
+        return calcular_digitos(numeroD, i + 1)
+
 
 opcion = 0
 while opcion != 6:
@@ -53,28 +58,27 @@ while opcion != 6:
                 print("Opcion no valida")
                 continue
             numeroB = int(input("Ingrese el segundo numero: "))
-            resultado = calcular_mcd(numeroA,numeroB)
+            resultado = calcular_mcd(numeroA, numeroB)
             print(f"resultado: {resultado}")
 
         case 2:
             print("=== repetir cadena ===")
             palabra = input("Ingrese una palabra: ")
             cantidad = int(input("Ingrese la cantidad que desea repetir: "))
-            cadena_repetir(cantidad,palabra)
+            cadena_repetir(cantidad, palabra)
 
         case 3:
             print("=== Cantidad de una letra en una palabra ===")
             cadena = input("Ingrese la cadena: ")
             letra = input("Ingrese la letra: ")
-            cantidad_de_letra = cantidad_letra(cadena,letra)
+            cantidad_de_letra = cantidad_letra(cadena, letra)
             print(f"Resultado: la letra: {letra} aparece {cantidad_de_letra} veces")
 
         case 4:
             print("=== Conversion decimal a binario ===")
-            numeroC=int(input("Ingrese un numero: "))
+            numeroC = int(input("Ingrese un numero: "))
             numero_binario = convertir_binario(numeroC)
             print(f"Resultao: {numero_binario}")
-
 
         case 5:
             print("=== Cantidad de digitos en un numero ===")
